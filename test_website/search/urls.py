5fcff06bb8f65ba5cf56c8c845140d38b01ede3c
REDSABRE\User <margat314>
2018-09-05 20:43:25 +1000

    Added Navbar
    - Navigation bar with relevant html
    - Added file directories for further use

--- test_website/search/urls.py
+++ test_website/search/urls.py
@@ -5,4 +5,7 @@ app_name = 'search'
 urlpatterns = [
     path('', views.home, name='home'),
     path('details/<int:id>/', views.car_detail, name='car_detail'),
+    path('about/', views.about, name='about'),
+    path('search/', views.about, name='search'),
+    path('locations/', views.about, name='locations'),
 ]
--- test_website/search/views.py
+++ test_website/search/views.py
@@ -4,6 +4,7 @@ from .models import Car
 from .forms import SearchForm
 
 # Create your views here.
+# search bar
 def home(request):
     if request.method == 'POST':
         form = SearchForm(request.POST)
@@ -27,3 +28,24 @@ def home(request):
 def car_detail(request, id):
     car = get_object_or_404(Car, pk=id)
     return HttpResponse("Details for car %s" % id)
+
+def about(request):
+    about = "About Us"
+    context = {
+        'title': about
+        }
+    return render(request, 'search/about.html', context)
+
+def search(request):
+    title = "Search"
+    context = {
+        'title': title
+        }
+    return render(request, 'search/search.html', context)
+
+def locations(request):
+    title = "Locations"
+    context = {
+        'title': title
+        }
+    return render(request, 'search/locations.html', context)

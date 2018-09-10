384e79e1d8b184b5c322afed12c05e35557d8835
Liam <liam.brock99@gmail.com>
2018-09-11 00:23:40 +1000

    Edit models, edit admin models

--- test_website/search/admin.py
+++ test_website/search/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from . models import Car, Customer, Store, Order
 # Register your models here.
-admin.site.register(Car)
-admin.site.register(Customer)
-admin.site.register(Store)
-admin.site.register(Order)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('car_make', 'car_model', 'car_series', 'car_series_year', 'car_price_new', 'car_engine_size', 'car_fuel_system', 'car_tank_capacity',
+                    'car_power','car_seating_capacity', 'car_standard_transmission', 'car_body_type', 'car_drive', 'car_wheelbase')
+    # list_filter = ('car_model', 'car_make', 'car_series_year')
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display =('customer_name', 'customer_address', 'customer_birthday', 'customer_occupation', 'customer_gender')
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('store_name', 'store_address', 'store_phone', 'store_city', 'store_state')
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_create_date', 'order_pickup_date', 'order_pickup_store', 'pickup_store_name', 'pickup_store_address', 'pickup_store_phone', 'pickup_store_city',
+                    'pickup_store_state', 'order_return_date', 'order_return_store', 'return_store_name', 'return_store_address', 'return_store_phone', 'return_store_city', 'return_store_state')
+
+admin.site.register(Car, CarAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(Order, OrderAdmin)
--- test_website/search/models.py
+++ test_website/search/models.py
@@ -18,10 +18,11 @@ class Car(models.Model):
     car_wheelbase = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.car_make
+        return self.car_make + ' ' + self.car_model
 
 class Customer(models.Model):
     customer_name = models.CharField(max_length=50)
+    customer_phone = models.IntegerField()
     customer_address = models.CharField(max_length=50)
     customer_birthday = models.DateField()
     customer_occupation = models.CharField(max_length=50)
@@ -41,4 +42,20 @@ class Store(models.Model):
         return self.store_name
 
 class Order(models.Model):
-    order_create_date = models.CharField(max_length=50)
+    order_create_date = models.DateField()
+
+    order_pickup_date = models.DateField()
+    order_pickup_store = models.IntegerField()
+    pickup_store_name = models.CharField(max_length=50)
+    pickup_store_address = models.CharField(max_length=50)
+    pickup_store_phone = models.IntegerField()
+    pickup_store_city = models.CharField(max_length=50)
+    pickup_store_state = models.CharField(max_length=50)
+
+    order_return_date = models.DateField()
+    order_return_store = models.IntegerField()
+    return_store_name = models.CharField(max_length=50)
+    return_store_address = models.CharField(max_length=50)
+    return_store_phone = models.IntegerField()
+    return_store_city = models.CharField(max_length=50)
+    return_store_state = models.CharField(max_length=50)

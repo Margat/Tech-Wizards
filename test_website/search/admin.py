45160e533d7ef1a2ab0029a98328d724ea392d0e
Liam <liam.brock99@gmail.com>
2018-09-11 23:02:35 +1000

    Search model changes, admin model changes

--- test_website/search/admin.py
+++ test_website/search/admin.py
@@ -2,18 +2,18 @@ from django.contrib import admin
 from . models import Car, Customer, Store, Order
 # Register your models here.
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('car_make', 'car_model', 'car_series', 'car_series_year', 'car_price_new', 'car_engine_size', 'car_fuel_system', 'car_tank_capacity',
+    list_display = ('car_id', 'car_make', 'car_model', 'car_series', 'car_series_year', 'car_price_new', 'car_engine_size', 'car_fuel_system', 'car_tank_capacity',
                     'car_power','car_seating_capacity', 'car_standard_transmission', 'car_body_type', 'car_drive', 'car_wheelbase')
     # list_filter = ('car_model', 'car_make', 'car_series_year')
 
 class CustomerAdmin(admin.ModelAdmin):
-    list_display =('customer_name', 'customer_address', 'customer_birthday', 'customer_occupation', 'customer_gender')
+    list_display =('customer_id', 'customer_name', 'customer_phone', 'customer_address', 'customer_birthday', 'customer_occupation', 'customer_gender')
 
 class StoreAdmin(admin.ModelAdmin):
-    list_display = ('store_name', 'store_address', 'store_phone', 'store_city', 'store_state')
+    list_display = ('store_id', 'store_name', 'store_address', 'store_phone', 'store_city', 'store_state')
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('order_create_date', 'order_pickup_date', 'order_pickup_store', 'pickup_store_name', 'pickup_store_address', 'pickup_store_phone', 'pickup_store_city',
+    list_display = ('order_id', 'order_create_date', 'order_pickup_date', 'order_pickup_store', 'pickup_store_name', 'pickup_store_address', 'pickup_store_phone', 'pickup_store_city',
                     'pickup_store_state', 'order_return_date', 'order_return_store', 'return_store_name', 'return_store_address', 'return_store_phone', 'return_store_city', 'return_store_state')
 
 admin.site.register(Car, CarAdmin)

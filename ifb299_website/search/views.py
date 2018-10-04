704d0613da3838ba5eb21b75b812d14029a177fd
Liam <liam.brock99@gmail.com>
2018-10-04 15:18:41 +1000

    changes to homepage query

--- ifb299_website/search/views.py
+++ ifb299_website/search/views.py
@@ -1,41 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from .models import Car
 from .forms import SearchForm
-from django.shortcuts import redirect
+
 
 # Creates home page with form
 def home(request):
     title = "Home"
-    if request.GET.get('query'):
+    if request.GET.get('query') or request.GET.get('min_price') or request.GET.get('max_price'):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
             min_price = form.cleaned_data['min_price']
             max_price = form.cleaned_data['max_price']
             year = form.cleaned_data['year']
+            ################################
             all_cars = Car.objects.all()
-            # If statements filter the results based on the inputs given
-            if not(query == None):
-                all_cars = all_cars.filter(car_make__icontains=query)
-            else:
-                print("Query: None")
-            if not(min_price == None):
-                all_cars = all_cars.filter(car_price_new__gte=min_price)
-            else:
-                print("Min_price: none")
-            if not(max_price == None):
-                all_cars = all_cars = all_cars.filter(car_price_new__lte=max_price)
-            else:
-                print("Min_price: none")
-            if (year == 'None'):
-                all_cars = all_cars.filter(car_series_year=year)
-            else:
-                print("Year field none")
+            if (query != None):
+                all_cars = all_cars.filter(car_make__icontains=query) | all_cars.filter(car_model__icontains=query) | all_cars.filter(car_series__icontains=query)
+            if (min_price != None and max_price != None):
+                all_cars = all_cars.filter(car_price_new__gte=min_price, car_price_new__lte=max_price)
     else:
             all_cars = Car.objects.all()
             form = SearchForm()
-    # variables to be parsed to the html template        
+    # variables to be parsed to the html template
     context = {
         'title': title,
         'form' : form,
